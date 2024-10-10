1fdcc66685bc12f4b4a616462a88c280a8f31a41
Gabriel <gabriel.schwartzmann@free.fr>
2024-10-10 17:21:47 +0200

    added half components and tweets.js reducer

---
+++ components/LastTweets.js
@@ -0,0 +1,19 @@
+import { useSelector } from 'react-redux';
+import Tweet from './Tweet';
+import styles from '../styles/LastTweets.module.css';
+
+function LastTweets() {
+  const tweetsList = useSelector((state) => state.tweets.value);
+
+  const tweets = tweetsList.map((data, i) => {
+    return <Tweet key={i} {...data} />;
+  });
+
+  return (
+    <>
+      {tweets}
+    </>
+  );
+}
+
+export default LastTweets;
